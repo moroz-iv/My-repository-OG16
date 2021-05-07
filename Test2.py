d28afa37cc634e4a10ef336f3dceb95be33a266a
Iryna Moroz <moroz9191777@gmail.com>
2021-05-07 14:44:08 +0300

    Так он високосный?

--- Test2.py
+++ Test2.py
@@ -1,3 +1,27 @@
-import calendar
+# Так он високосный?
+year = int(input("Please, enter the year : "))
 
-print(calendar.month(2020, 9))
+
+def x(y):
+    if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
+        return "It's a leap year!"
+    else:
+        return "This year is not leap year!"
+
+
+print(x(year))
+
+
+# year = int(input("Please, enter the year : "))
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#    print("It's a leap year!")
+# else:
+#    print("This year is not leap year!")
+
+
+# import calendar
+# month = int(input("Please, enter the month : "))
+# calendar.setfirstweekday(calendar.sunday)
+# my cal = calendar.month(year, month)
+# my cal = calendar.calendar(year)
+# print(my cal)
